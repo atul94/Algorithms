9be4f240ab13e0d89c2309c82ab0e656a621c286
atul94 <atulhyd2013@outlook.com>
2018-10-14 23:16:53 +0530

    Create PreOrderTraversal.cpp

---
+++ Trees/PreOrderTraversal.cpp
@@ -0,0 +1,32 @@
+/*
+@Description Given a binary tree, return the preorder traversal of its nodes’ values.
+*/
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+vector<int> Solution::preorderTraversal(TreeNode* A) {
+    vector <int> sol;
+    if(A == NULL){
+        return sol;
+    }
+    vector <TreeNode *> st;
+    while(A || !st.empty()){
+        if(A){
+            sol.push_back(A->val);
+            st.push_back(A);
+            A = A->left;
+        }else{
+            TreeNode * temp;
+            temp = st.back();
+            A = temp->right;
+            st.pop_back();
+        }
+    }
+    return sol;
+}
