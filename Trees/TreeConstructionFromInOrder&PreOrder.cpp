f831040773eadf3a7c3331f59fd6e4e28ea3b31d
atul94 <atulhyd2013@outlook.com>
2018-10-17 11:43:41 +0530

    Create TreeConstructionFromInOrder&PreOrder.cpp

---
+++ Trees/TreeConstructionFromInOrder&PreOrder.cpp
@@ -0,0 +1,39 @@
+/**
+@Description Given preorder and inorder traversal of a tree, construct the binary tree.
+Note: You may assume that duplicates do not exist in the tree. 
+* Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+*/
+TreeNode* Solution::buildTree(vector<int> &preorder, vector<int> &inorder) {
+    vector<TreeNode *> st;
+    int ptrInOrder = 0;
+    int ptrPreOrder = 0;
+    TreeNode * root;
+    TreeNode * temp;
+    root = new TreeNode(preorder[ptrPreOrder++]);
+    st.push_back(root);
+    while(ptrPreOrder < preorder.size()){
+        if(st.back()->val == inorder[ptrInOrder]){
+            temp = st.back();
+            st.pop_back();
+            ptrInOrder++;
+            if(!st.empty() && st.back()->val == inorder[ptrInOrder]){
+                continue;
+            }else{
+                temp->right = new TreeNode(preorder[ptrPreOrder++]);
+                st.push_back(temp->right);
+            }
+            
+        }else{
+            temp = new TreeNode(preorder[ptrPreOrder++]);
+            st.back()->left = temp;
+            st.push_back(temp);
+        }
+    }
+    return root;
+}
