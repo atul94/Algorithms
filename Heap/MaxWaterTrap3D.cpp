fe43edd0026802fd34490de40509382397ab05e0
Atul Agarwal <atulhyd2013@outlook.com>
2020-02-23 15:55:43 +0530

    Update MaxWaterTrap3D.cpp

--- Heap/MaxWaterTrap3D.cpp
+++ Heap/MaxWaterTrap3D.cpp
@@ -1,4 +1,6 @@
 /**
+https://leetcode.com/problems/trapping-rain-water-ii/
+
 Given an m x n matrix of positive integers representing the height of each unit cell in a 2D elevation map, compute the volume of water it is able to trap after raining.
 
  
